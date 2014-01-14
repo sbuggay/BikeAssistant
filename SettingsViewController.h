8e6797475235f804afb6d57eca9d00f543ece09c
sbuggay <sbuggay@gmail.com>
2014-01-13 22:36:27 -0600

    Added tab bar view controller and settings

---
+++ SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  BikeAssistant
+//
+//  Created by Devan Buggay on 1/13/14.
+//  Copyright (c) 2014 Devan Buggay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UITableViewController
+
+@end
