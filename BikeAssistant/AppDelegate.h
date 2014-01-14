ae4982c1f834ff8d4fb50f6fa81982188060af89
sbuggay <sbuggay@gmail.com>
2014-01-13 21:24:49 -0600

    Initial commit

---
+++ BikeAssistant/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BikeAssistant
+//
+//  Created by Devan Buggay on 1/13/14.
+//  Copyright (c) 2014 Devan Buggay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
