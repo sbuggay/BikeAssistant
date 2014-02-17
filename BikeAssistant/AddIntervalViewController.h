8c0783807f29df2ad17e04a5c55bcc77947e3de7
Chauncey Philpot <cbp0009@csse-imac3.cse.eng.auburn.edu>
2014-02-17 17:08:45 -0600

    Working on IntervalTimer. Not much done.

--- BikeAssistant/AddIntervalViewController.h
+++ BikeAssistant/AddIntervalViewController.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 
 #import "Interval.h"
+#import "CreateIntervalViewController.h"
 
 @interface AddIntervalViewController : UIViewController
 {
--- BikeAssistant/AddIntervalViewController.m
+++ BikeAssistant/AddIntervalViewController.m
@@ -8,10 +8,36 @@
 
 #import "AddIntervalViewController.h"
 
+
 @implementation AddIntervalViewController
+
+- (IBAction)finishedButton:(id) sender {
+    
+    
+    //Check to see if name and time were inputted
+    //Verify name is unique to THIS interval
+    //Update Interval object
+    //Display message that adding was successful
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Success" message:@"Interval Added Successfully. Press back if you're done." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    
+    [alert show];
+    
+       
+    //Prepare for additional timers by clearing inputs
+}
 
 -(void) viewDidLoad {
     self.interval.intervalName = @"test2";
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    
+    
+    
+    
+   
+    
 }
 
 @end
