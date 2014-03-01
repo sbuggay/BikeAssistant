eaf52f00d4671220e98de3d55a1d93a20a19647c
Devan Buggay <sbuggay@gmail.com>
2014-03-01 18:56:48 -0500

    Updated pod file

--- Podfile
+++ Podfile
@@ -1,3 +1,7 @@
 platform :ios, '7.0'
+
+inhibit_all_warnings!
+
 pod 'SWRevealViewController', '~>1.1'
 pod 'GPXParser', '~>1.0'
+pod 'InAppSettingsKit', '~>2.0.1'
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -12,6 +12,12 @@
 #define COCOAPODS_VERSION_MINOR_GPXParser 0
 #define COCOAPODS_VERSION_PATCH_GPXParser 0
 
+// InAppSettingsKit
+#define COCOAPODS_POD_AVAILABLE_InAppSettingsKit
+#define COCOAPODS_VERSION_MAJOR_InAppSettingsKit 2
+#define COCOAPODS_VERSION_MINOR_InAppSettingsKit 0
+#define COCOAPODS_VERSION_PATCH_InAppSettingsKit 1
+
 // SWRevealViewController
 #define COCOAPODS_POD_AVAILABLE_SWRevealViewController
 #define COCOAPODS_VERSION_MAJOR_SWRevealViewController 1
